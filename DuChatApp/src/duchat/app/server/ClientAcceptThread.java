5fd20120659cec7b5e63fe31b591606da6fee2a4
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-10-29 15:32:33 +0300

    stop action

--- DuChatApp/src/duchat/app/server/ClientAcceptThread.java
+++ DuChatApp/src/duchat/app/server/ClientAcceptThread.java
@@ -5,14 +5,10 @@
  */
 package duchat.app.server;
 
-import duchat.entity.User;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  *
@@ -21,11 +17,11 @@ import java.net.Socket;
 public class ClientAcceptThread implements Runnable {
 
     private Thread thread;
+    private final AtomicBoolean running = new AtomicBoolean(false);
     private final String threadName;
     private ServerSocket serverSocket = null;
     private Socket clientSocket = null;
     private final ChatServer chatServer;
-    User clientPacket;
 
     public ClientAcceptThread(ChatServer duChatServer) throws IOException {
         this.chatServer = duChatServer;
@@ -36,39 +32,34 @@ public class ClientAcceptThread implements Runnable {
     // implement a run() method provided by a Runnable interface
     @Override
     public void run() {
-        while (true) {
+        running.set(true);
+        while (running.get()) {
             try {
                 clientSocket = this.serverSocket.accept();
-                System.out.println("New User Connected");
-                InputStream inputStream = this.clientSocket.getInputStream();
-                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-                OutputStream outputStream = this.clientSocket.getOutputStream();
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-                // TO-DO make client side send the user object or transport packet include user object to store user names
-                
-                clientPacket = (User) objectInputStream.readObject();
                 ClientHandlerThread newUserThread = new ClientHandlerThread(this.clientSocket, this.chatServer);
-                chatServer.addUserThread(newUserThread);
+                this.chatServer.addUserThread(newUserThread);
                 newUserThread.start();
-                chatServer.increaseUserCount();
-                //System.out.println(chatServer.getUserCount());
+                this.chatServer.increaseUserCount();
+
                 //chatServer.addUserName("incoming username from clientside");
-
-            } catch (IOException | ClassNotFoundException ex) {
-
+            } catch (IOException  ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
 
     public void start() {
-        System.out.println("Server Thread Started to Listen Socket for New Users.");
+        System.out.println(threadName + "-" + chatServer.getServerId() + ": Client Accept Thread Started.");
         if (thread == null) {
             // instantiate a Thread object
             thread = new Thread(this, threadName);
             // start it by calling start() method, which executes a call to run() method.
             thread.start();
         }
+    }
 
+    public void stop() {
+        running.set(false);
     }
 }
