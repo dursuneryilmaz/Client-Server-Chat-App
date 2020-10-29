1c545296cbdfa542b5e37d84e189ca8c92a403c9
Dursun ERYILMAZ <dursun1147@gmail.com>
2020-10-29 15:33:41 +0300

    stop, client message delivering and loading old messages actions implemented

--- DuChatApp/src/duchat/app/server/ClientHandlerThread.java
+++ DuChatApp/src/duchat/app/server/ClientHandlerThread.java
@@ -6,12 +6,16 @@
 package duchat.app.server;
 
 import duchat.entity.Message;
+import duchat.entity.User;
+import duchat.service.MessageService;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,58 +26,75 @@ import java.util.logging.Logger;
 public class ClientHandlerThread implements Runnable {
 
     Thread thread;
+    User user;
+    private final AtomicBoolean running = new AtomicBoolean(false);
     private final ChatServer chatServer;
     private Socket clientSocket = null;
     ObjectInputStream objectInputStream;
     ObjectOutputStream objectOutputStream;
+    MessageService messageService;
     Message clientPacket;
-    Message serverPacket;
 
     public ClientHandlerThread(Socket clientSocket, ChatServer server) {
         this.clientSocket = clientSocket;
         this.chatServer = server;
+        this.messageService = new MessageService();
     }
 
     // implement a run() method provided by a Runnable interface
     @Override
     public void run() {
         try {
-            InputStream inputStream = this.clientSocket.getInputStream();
-            OutputStream outputStream = this.clientSocket.getOutputStream();
 
-            objectInputStream = new ObjectInputStream(inputStream);
+            OutputStream outputStream = this.clientSocket.getOutputStream();
             objectOutputStream = new ObjectOutputStream(outputStream);
-           /*  TO-DO List
-            * Get the incoming transport object(message) from objectInpputStream
-            * Store the message to database 
-            * Send the message to all clients using servers broadcast() method
-            * Determine the user logout status 
-            * if client leave remove username and clientThread from server using servers removeUser() method
-            * Inform all clients about leave
-            * close socket
-            *
-            */
-            
-            
-            
-            
-        } catch (IOException ex) {
+            objectOutputStream.writeObject(new Message(0, chatServer.getServerId(), "ChatServer", 0, "Connection Successfull.", ""));
+
+            InputStream inputStream = this.clientSocket.getInputStream();
+            objectInputStream = new ObjectInputStream(inputStream);
+            user = (User) objectInputStream.readObject();
+
+            ArrayList<Message> lastMessages = messageService.getMessages(user, chatServer.getServer());
+            for (Message m : lastMessages) {
+                objectOutputStream.writeObject(m);
+            }
+
+            running.set(true);
+            while (running.get()) {
+                clientPacket = (Message) objectInputStream.readObject();
+
+                if (!clientPacket.getText().equals("/bye")) {
+                    messageService.saveMessage(clientPacket);
+                    chatServer.broadcast(clientPacket, this);
+                } else {
+                    break;
+                }
+            }
+
+        } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(ClientHandlerThread.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
     }
 
     public void start() {
-        System.out.println("Messaging Thread Start to Run for Particular User.");
         if (thread == null) {
-            // instantiate a Thread object
             thread = new Thread(this);
-            // start it by calling start() method, which executes a call to run() method.
             thread.start();
+            System.out.println(chatServer.getServerName() + "-" + chatServer.getServerId() + ": Client Handler Thread Started.");
         }
     }
 
-    void sendMessage(String message) {
+    public void stop() {
+        running.set(false);
+    }
 
+    void sendMessage(Message message) {
+        try {
+            objectOutputStream.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandlerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     void printUsers() {
